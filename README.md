7ea30c9658fc1c85ac137d8c81f6344f61a68b04
RenataMartins <163059025+RenataGabrielle@users.noreply.github.com>
2024-11-24 13:00:47 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Api-livros
+Created with CodeSandbox
---
+++ src/components/Estante/EstanteLivros/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css"
+
+import BoxLivros from "../BoxLivros/index"
+
+export default function EstanteLivros({livrosFiltrados, handleEstadoLivro }) { 
+    return (
+        <div className="listaLivros">
+            {livrosFiltrados.map(livro => (
+                <BoxLivros 
+                    key={livro.id}
+                    livro={livro}
+                    handleEstadoLivro={handleEstadoLivro}
+                />
+            ))}
+        </div>
+    )
+}
+    
